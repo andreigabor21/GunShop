9839968035ff2ad8608491477e80db5d9b8ca6c4
Andrei <andreigabor36@gmail.com>
2021-04-24 20:01:15 +0300

    Need redux

--- gun-shop-ui/src/app/clients/client-new/client-new.component.ts
+++ gun-shop-ui/src/app/clients/client-new/client-new.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Client} from '../shared/client.model';
 import {ClientService} from '../shared/client.service';
 import {Router} from '@angular/router';
-import {FormBuilder, FormControl} from '@angular/forms';
+import {FormBuilder, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-client-new',
@@ -16,7 +16,7 @@ export class ClientNewComponent implements OnInit {
               private router: Router) { }
 
   clientForm = this.formBuilder.group({
-    name: '',
+    name: ['', Validators.required],
     dateOfBirth: '',
   });
 
@@ -27,6 +27,10 @@ export class ClientNewComponent implements OnInit {
     this.clientService.saveClient(this.clientForm.value)
       .subscribe(() => this.router.navigateByUrl('/clients'));
     this.clientForm.reset();
+  }
+
+  public hasError = (controlName: string, errorName: string) => {
+    return this.clientForm.controls[controlName].hasError(errorName);
   }
 
 }
